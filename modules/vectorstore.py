fa4ee1ab9e2fdb8fcd0c4453aa557c4b78e8b8b3
alihaider1001 <139347700+alihaider1001@users.noreply.github.com>
2025-07-09 15:15:55 +0500

    Update vectorstore.py

--- modules/vectorstore.py
+++ modules/vectorstore.py
@@ -1,8 +1,8 @@
-from langchain.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceInstructEmbeddings
 
 def create_vectorstore(pages):
-    embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/paraphrase-MiniLM-L3-v2"
+    embeddings = HuggingFaceInstructEmbeddings(
+        model_name="hkunlp/instructor-xl"  # You can also try instructor-large or instructor-base
     )
     return FAISS.from_documents(pages, embeddings)
