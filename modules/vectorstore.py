5fe5fc67d26e7d6ff318e10dedae30937748fa9b
alihaider1001 <139347700+alihaider1001@users.noreply.github.com>
2025-07-09 15:49:35 +0500

    Switched to HuggingFaceInstructEmbeddings and added missing dependency

--- modules/vectorstore.py
+++ modules/vectorstore.py
@@ -3,6 +3,6 @@ from langchain_community.embeddings import HuggingFaceInstructEmbeddings
 
 def create_vectorstore(pages):
     embeddings = HuggingFaceInstructEmbeddings(
-        model_name="hkunlp/instructor-xl"  # You can also try instructor-large or instructor-base
+        model_name="hkunlp/instructor-base"
     )
     return FAISS.from_documents(pages, embeddings)
